e4c1834baad0f970e38824a2985c001fba719a9c
Radhika Luvani <radhikaluvani3@gmail.com>
2020-09-05 19:06:35 +0530

    Add files via upload

--- daily-programs/20200905/guess_game.py
+++ daily-programs/20200905/guess_game.py
@@ -1,1 +1,16 @@
-
+import random
+num = random.randint(1,9)
+count = 0
+guess = input('Enter your number between 1 to 9:')
+guess = int(guess)
+while guess!= num and guess!='exit':
+	guess = int(guess)
+	count+=1
+	if guess > num:
+		guess = input('Number is less than you have guessed')
+	elif guess < num:
+		guess = input('Number is bigger than you have guessed')
+if guess == num:
+	print('voilla! Number you have guessed is ' +str(count))
+else :
+	print('Try again ')
