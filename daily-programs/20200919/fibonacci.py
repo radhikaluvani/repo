cebdd916f62494e978c37db4007981a29935bd5c
Radhika Luvani <radhikaluvani3@gmail.com>
2020-09-19 21:33:09 +0530

    Add files via upload

--- daily-programs/20200919/fibonacci.py
+++ daily-programs/20200919/fibonacci.py
@@ -1,1 +1,7 @@
-
+num = int(input('Enter any two numbers to star '))
+a = int(input())
+b = int(input('Fibonnaci number to generated:'))
+list = [num,a]
+for x in range(0,b,1):
+	list.append(list[x]+list[x+1])
+print(list)
