9fbddd2a731a446d87db527a829e8dca8b564660
Radhika Luvani <radhikaluvani3@gmail.com>
2020-09-29 21:47:42 +0530

    Add files via upload

--- daily-programs/20200929/genrate_password.py
+++ daily-programs/20200929/genrate_password.py
@@ -1,1 +1,9 @@
+import random
+import string
 
+def get_password(length):
+    password = string.ascii_letters + string.digits + string.punctuation
+    password1 = ''.join(random.choice(password) for i in range(length))
+    print("Your random password is:", password1)
+
+get_password(6)
