214a55b6fcb343cc7b98ea6b5c742fc11e3c30a7
Radhika Luvani <radhikaluvani3@gmail.com>
2020-09-30 22:17:42 +0530

    Add files via upload

--- daily-programs/20200930/newyork_articles.py
+++ daily-programs/20200930/newyork_articles.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+url = 'http://www.nytimes.com'
+r = requests.get(url)
+r_html = r.text
+soup = BeautifulSoup(r_html, "lxml")
+
+for titles in soup.find_all(class_ = "story"):
+	title = titles.a
+	print(title.string)
 
