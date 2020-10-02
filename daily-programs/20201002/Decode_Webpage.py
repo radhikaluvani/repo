92edbef3445ef0fe5e5c8afd2e3d58ef0203389e
Radhika Luvani <radhikaluvani3@gmail.com>
2020-10-02 16:48:54 +0530

    Add files via upload

--- daily-programs/20201002/Decode_Webpage.py
+++ daily-programs/20201002/Decode_Webpage.py
@@ -1,1 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.nytimes.com/"
+r = requests.get(url).text
+
+soup = BeautifulSoup(r,'lxml')
+
+for article in soup.find_all("article"):
+    headline = article.h2
+    try:
+        print(headline.get_text())
+    except Exception as e:
+        print(None)
 
