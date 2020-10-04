c091143b380173327de8d1f06b8cafeb4aa9e0a0
Radhika Luvani <radhikaluvani3@gmail.com>
2020-10-04 19:44:28 +0530

    Add files via upload

--- daily-programs/20201004/Decode_Webpage2.py
+++ daily-programs/20201004/Decode_Webpage2.py
@@ -1,1 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
 
+url = "https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+article = soup.select('div p')
+with open("article.txt", "w") as file:
+	for each in article:
+		file.write(each.text)
+print("File is created")
